fa4a8755e3fb0984d7f8750157a670dd2b20e894
André Alonso da Silva <andrealonso93@gmail.com>
2021-12-30 22:05:02 -0300

    Creating projects (Domain, Repository and Service) Input entity Repository base + Input repository.

---
+++ Journal.Repository/InputRepository.cs
@@ -0,0 +1,37 @@
+﻿using Journal.Domain;
+using Microsoft.Extensions.Configuration;
+
+namespace Journal.Repository;
+
+/// <summary>
+/// Repository to mange database operations of journal inputs
+/// </summary>
+public class InputRepository : BaseRepository, IRepository<Input>
+{
+    public InputRepository(IConfiguration config) : base(config) { }
+
+    public bool Delete(int objectId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Input Find(int objecId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Insert(Input insertObject)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Input> List()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update(Input updateObject)
+    {
+        throw new NotImplementedException();
+    }
+}
