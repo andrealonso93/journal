d46420ded33bc6495b01532fefac078077d2d705
André Alonso da Silva <andrealonso93@gmail.com>
2022-01-03 17:22:55 -0300

    Adding user constructor.

--- Journal.Domain/User.cs
+++ Journal.Domain/User.cs
@@ -2,6 +2,12 @@ namespace Journal.Domain
 {
     public class User
     {
+        public User(string name)
+        {
+            Name = name;
+            CreationDateTime = DateTime.Now;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime CreationDateTime { get; set; }
