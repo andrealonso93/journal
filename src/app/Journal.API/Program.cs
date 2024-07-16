7118124b657a9ecee04150364202a628ab6690b0
André Alonso da Silva <andre.silva@agilecontent.com>
2024-07-16 15:21:13 -0300

    Adding entity framework core and initial database creation migration

--- src/app/Journal.API/Program.cs
+++ src/app/Journal.API/Program.cs
@@ -1,3 +1,6 @@
+using Journal.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<InputContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("journal")));
 
 var app = builder.Build();
 
---
+++ src/app/Journal.Database/InputContext.cs
@@ -0,0 +1,12 @@
+﻿using Journal.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journal.Database
+{
+    public class InputContext : DbContext
+    {
+        public InputContext(DbContextOptions<InputContext> options) : base(options) { }
+
+        public DbSet<Input> Inputs => Set<Input>();
+    }
+}
