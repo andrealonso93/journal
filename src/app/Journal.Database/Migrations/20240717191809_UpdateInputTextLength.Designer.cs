44cafbd64a72d45d3c1cbb62e8d43e3ef71403c0
André Alonso da Silva <andrealonso93@gmail.com>
2024-07-17 17:06:10 -0300

    Fixing database migratio for input to have maxlength

--- src/app/Journal.Database/Migrations/20240717191809_UpdateInputTextLength.Designer.cs
+++ src/app/Journal.Database/Migrations/20240717191809_UpdateInputTextLength.Designer.cs
@@ -4,6 +4,7 @@ using Journal.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Journal.Database.Migrations
 {
     [DbContext(typeof(InputContext))]
-    partial class InputContextModelSnapshot : ModelSnapshot
+    [Migration("20240717191809_UpdateInputTextLength")]
+    partial class UpdateInputTextLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
