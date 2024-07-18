d6fae2d07aee553505c76f2ea559180d3187cdf6
André Alonso da Silva <andrealonso93@gmail.com>
2024-07-18 17:16:52 -0300

    Adding log and primary constructor to notification filter

--- src/app/Journal.API/NotificationFilter.cs
+++ src/app/Journal.API/NotificationFilter.cs
@@ -4,19 +4,17 @@ using System.Text.Json;
 
 namespace Journal.API
 {
-    public class NotificationFilter : IActionFilter
+    public class NotificationFilter(NotificationContext _notificationContext, ILogger<NotificationFilter> _logger) : IActionFilter
     {
-        private readonly NotificationContext _notificationContext;
-
-        public NotificationFilter(NotificationContext notificationContext)
-        {
-            _notificationContext = notificationContext;
-        }
-
+        private readonly NotificationContext _notificationContext = _notificationContext;
+        private readonly ILogger<NotificationFilter> _logger = _logger;
+        
         public async void OnActionExecuted(ActionExecutedContext context)
         {
             if (_notificationContext.HasNotifications)
             {
+                _logger.LogError("Request failed with notifications. The first added notification will be the response");
+
                 var firstNotification = _notificationContext.Notifications.First();
                 context.HttpContext.Response.StatusCode = (int)firstNotification.HttpStatusCode;
                 context.HttpContext.Response.ContentType = "application/json";
