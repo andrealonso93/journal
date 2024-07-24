6b912e84f1d9542ee28e1f1566ca82846b5adfb5
André Alonso da Silva <andrealonso93@gmail.com>
2024-07-24 12:37:26 -0300

    Creation of service unit tests project

---
+++ src/tests/Journal.Service.Tests/InputServiceTests.cs
@@ -0,0 +1,63 @@
+using Journal.Domain;
+using Journal.Notification;
+using Journal.Repository;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+
+namespace Journal.Service.Tests
+{
+    public class InputServiceTests
+    {
+        private readonly IInputService _inputService;
+        private readonly InputRepository _inputRepository = Substitute.For<InputRepository>();
+
+        public InputServiceTests()
+        {
+            _inputService = new InputService(_inputRepository, Substitute.For<ILogger<InputService>>(), Substitute.For<NotificationContext>());
+        }
+
+        [Fact]
+        public async Task Should_UpdateExistingRecord()
+        {
+            // Arrange
+            var existingInput = new Input { Id = 1, InputText = "Test" };
+            _inputRepository.Find(1).Returns(existingInput);
+            var updatedInput = existingInput;
+            updatedInput.InputText = "Updated Test";
+            
+            // Act
+            var response = await _inputRepository.Update(updateInput);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.Equal(response.InputText, updateInput.InputText);
+        }
+
+        [Fact]
+        public async Task Should_ReturnNull_WhenInputTextIsTooLong()
+        {
+            // Arrange
+
+
+            // Act
+
+
+            // Assert
+
+        }
+
+
+        [Fact]
+        public async Task Should_RetunNull_WhenRecord_DoesntExist()
+        {
+            // Arrange
+
+
+            // Act
+
+
+            // Assert
+
+        }
+    }
+}
